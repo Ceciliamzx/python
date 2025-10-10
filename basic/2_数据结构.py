cf77244e5c8c0a28904442a2bbc951f041c7ab2a
Cecilia <759889493@qq.com>
2025-10-11 04:52:28 +0800

    1

--- basic/2_数据结构.py
+++ basic/2_数据结构.py
@@ -244,3 +244,12 @@ with open(".\data.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
     for line in lines:
         print(line)
+
+with open(".\poem.txt", "w", encoding="utf-8") as f:
+    f.write("我欲乘风归去\n")
+    f.write("又恐琼楼玉宇\n")
+    f.write("高处不胜寒\n")
+
+with open(".\poem.txt", "a", encoding="utf-8") as f:
+    f.write("起舞弄清影\n")
+    f.write("何似在人间\n")
