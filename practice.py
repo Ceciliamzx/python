3832eeb70bd8eb68497e3717cd674d9bf5fcb6e1
Cecilia <759889493@qq.com>
2025-10-15 07:28:48 +0800

    1

--- practice.py
+++ practice.py
@@ -28,6 +28,7 @@ lst3 = lst1 + lst2
 lst3.sort()
 print(lst3)
 
+# 6.control()
 class HomeAppliance:
     def control(self):
         pass
@@ -51,7 +52,7 @@ operate_appliance(tv)
 operate_appliance(ac)
 operate_appliance(light)
 
-
+# 7.yieling()
 class Animal:
     def yieling(self):
         pass
@@ -69,5 +70,53 @@ cat1 = Cat()
 make_animal_sound(dog1)
 make_animal_sound(cat1)
 
+ # 8.两数之和
+number1 = 1
+number2 = 2
+sum = number1 + number2
+print(sum)
+print(f"{number1}+{number2}={sum}")
 
+# 9.数字的阶乘
+def get_jiecheng(number):
+    result = 1
+    while number > 0:
+        result *= number
+        number -=1
+    return result
+print("jiecheng 6=",get_jiecheng(6))
 
+# 10.计算圆的面积
+import math
+print(math.pi)
+
+import math
+def compute_area_of_circle(r):
+    return round(math.pi * r * r,2)
+print("area of 2 is:",compute_area_of_circle(2))
+
+# 11.区间内的所有素数
+def is_primes(number):
+    if number in (1,2):
+        return True
+    for idx in range (2,number):
+        if number % idx == 0 :
+            return False
+        return True
+
+def print_primes(begin, end):
+    for number in range(begin,end+1):
+        if is_primes(number):
+            print(f"{number}is a primes")
+
+begin = 11
+end = 25
+print_primes(begin,end)
+
+# 12.求前n个数字的平方和
+def sum_of_square(n):
+    result = 0
+    for number in range (1,n+1):
+        result += number * number
+    return result
+print(sum_of_square(5))
